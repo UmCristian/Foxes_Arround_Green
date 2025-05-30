2ca8baed85d3bc8240811b143c9a390d12e45b4b
UmCristian <cristiansalazar.gm@academia.umb.edu.co>
2025-05-30 13:51:46 -0500

    usando otros imports para el main.js.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
-import { ARButton } from 'https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/webxr/ARButton.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.150.1/build/three.module.js';
+import { ARButton } from 'https://unpkg.com/three@0.150.1/examples/jsm/webxr/ARButton.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders/GLTFLoader.js';
 
 let camera, scene, renderer;
 let controller;
